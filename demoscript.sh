897f469fea5434350a23a3c673317f37cc1fe724
vijay-2312 <viijaynagaraju@gmail.com>
2026-04-01 05:03:45 -0700

    to create conflict

---
+++ demoscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Author - vijay
+#Date - 31/03/2026
+#Description - Example shell script
+
+ls -lrth | wc -l
+
+touch sampletext
---
+++ testscript.sh
@@ -0,0 +1,12 @@
+!/bin/bash
+#Author - vijay
+#Date - 31/03/2026
+#Description - Example shell script
+
+echo "Enter your filename"
+read filename
+
+echo "the file name is" $filename
+
+touch $filename
+chmod 777 $filename
