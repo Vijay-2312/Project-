ec3dc59e814f9e91d5721ea4f9289e3365bdd6b4
vijay-2312 <viijaynagaraju@gmail.com>
2026-04-12 09:14:09 -0700

    changes

--- demoscript.sh
+++ demoscript.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 #Author - vijay
-#Date - 31/03/2026
+#Date - 03/04/2026
 #Description - Example shell script
 
-echo "Difference of two numbers"
-sum=$(( 70 - 40))
-echo "Differnce of two num" $sum
-echo "the file name is"$0
-echo $#
-echo $@
-echo $?
+echo "Select an option (1-5):"
+echo "1. Check Memory (free -m)"
+echo "2. Check Disk Space (df -h)"
+echo "3. Check Directory Usage (du -sh)"
+echo "4. System Information (uname -a)"
+echo "5. Help (man --help)"
 
+read choice
+
+case $choice in
+1)
+echo "Displaying Memory Usage:"
+free -m
+;;
+2)
+echo "Displaying Disk Space:"
+df -h
+;;
+3)
+echo "Displaying Directory Usage (Current Folder):"
+du -sh .
+;;
+4)
+echo "Displaying System Info:"
+uname -a
+;;
+5)
+echo "Displaying Help for Manual Pages:"
+man --help
+;;
+*)
+echo "Invalid selection. Please choose 1-5."
+;;
+esac
